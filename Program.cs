ce3f95fbb177d185576aae75a718b44bdb11e710
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-04-01 16:52:58 +0500

    HW-31-03-20

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+namespace MyprojecsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+                                    //Задача 1
+            Console.Write("N = ");
+            int N = int.Parse(Console.ReadLine());
+            int[] arrey = new int[N];
+            for (int i = 0; i < arrey.Length; i++)
+            {
+                arrey[i] = new Random().Next(100);
+                Console.Write($"{arrey[i]}  ");
+            }
+            int MaxValue = arrey.Max();
+            Console.WriteLine($"\n MaxValue = {MaxValue}");
+            int MinValue = arrey.Min();
+            Console.WriteLine($" MinValue = {MinValue}");
+            int SumValues=arrey.Sum();
+            Console.WriteLine($" SumValues = {SumValues}");
+            double Aver=arrey.Average();
+            Console.WriteLine($" Average = {Aver}");
+            for(int i=0;i<arrey.Length;i++)
+            {
+                if(arrey[i]%2==1)
+                {
+                    Console.Write($"{arrey[i]}  ");
+                }
+            }
+            Console.ReadKey();
+
+                                        //Задача 2
+           int[] mas = new int[10];
+           Console.WriteLine("\t\t\tМассив");
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = new Random().Next(1, 20);
+                Console.Write($"{mas[i]}  ");
+            }
+            Console.WriteLine();
+            Array.Reverse(mas);
+            Console.WriteLine("\t\t\tВ обратном порядке");
+            for (int i = 0; i < mas.Length; i++)
+            {
+                 Console.Write($"{mas[i]}  ");
+            }
+            Console.ReadKey();
+                                        //Задача 3
+            Console.Write("N = ");
+            int N = int.Parse(Console.ReadLine());
+            int[] arrey = new int[N];
+            for (int i = 0; i < arrey.Length; i++)
+            {
+                arrey[i] = new Random().Next(100);
+                Console.Write($"{arrey[i]}  ");
+            }
+            Console.Write("\ncount: ");
+            int count = int.Parse(Console.ReadLine());
+            Console.Write("index: ");
+            int index = int.Parse(Console.ReadLine());
+            Console.WriteLine("\t\t\tНовый массив");
+            int[] arrey2 = new int[count];
+            for (int i = 0; i < arrey2.Length; i++)
+            {
+                if ((index + i) < N)
+                {
+                    arrey2[i] = arrey[index + i];
+                    Console.Write($"{arrey2[i]}  ");
+                }
+                else
+                {
+                    arrey2[i] = 1;
+                    Console.Write($"{arrey2[i]}  ");
+                }
+            }
+            Console.ReadKey();*/
+        }
+    }
+}
